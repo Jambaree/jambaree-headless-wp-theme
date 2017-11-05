dc23fcf37275f8b62cc1c6114577c383e782c68d
Tyler Barnes <tyler@bare.ca>
2017-11-05 13:06:51 -0800

    Move php to includes

---
+++ inc/accessibility/skiplinks.php
@@ -0,0 +1,6 @@
+<!-- http://rianrietveld.com/2015/04/04/the-screen-reader-text-class-why-and-how/ -->
+<ul class="skip-link">
+ <li><a href="#nav" class="screen-reader-text">Jump to main navigation</a></li>
+ <li><a href="#content" class="screen-reader-text">Jump to content</a></li>
+ <li><a href="#footer" class="screen-reader-text">Jump to footer</a></li>
+</ul>
---
+++ inc/modules/burger.php
@@ -0,0 +1,13 @@
+<div class="hamburger__layout">
+  <button class="main-menu__button hamburger hamburger--elastic" type="button">
+    <span class="hamburger-box">
+      <span class="hamburger-inner"></span>
+    </span>
+    <span class="hamburger-title__container">
+      <span class="hamburger-title__inner">
+        <span class="hamburger-title">menu</span>
+        <span class="hamburger-title">close</span>
+      </span>
+    </span>
+  </button>
+</div>
