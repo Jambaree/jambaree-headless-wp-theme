b7004f92472437762db1876c49e9c933e86e8949
Tyler Barnes <tyler@bare.ca>
2017-11-05 09:59:14 -0800

    Move file src vars to top. Remove ie 9 and 10 prefixes. Remove singularitygs.

--- gulpfile.js
+++ gulpfile.js
@@ -8,21 +8,31 @@ var project                 = 'PRP', // Project Name.
     projectURL              = 'prp.dev', // Project URL. Could be something like localhost:8888.
     productURL              = './'; // Theme URL. Leave it like it is, since our gulpfile.js lives in the root
 
+var styleWatchFiles         = 'sass/**/*.scss', // Path to all *.scss files inside css folder and inside them.
+    styleSRC                = '*.css',
+    customJSWatchFiles      = 'js/*.js', // Path to all custom JS files.
+    projectPHPWatchFiles    = '**/*.php'; // Path to all PHP files.
+
 //
 //
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var importer = require('node-sass-globbing');
-var plumber = require('gulp-plumber');
-var browserSync = require('browser-sync').create();
-var cssmin = require('gulp-cssmin');
-var uncss = require('gulp-uncss');
-var stripCssComments = require('gulp-strip-css-comments');
-var uglify = require('gulp-uglify');
-var cleanCSS = require('gulp-clean-css');
+
+//
+// Dependencies
+//
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    importer = require('node-sass-globbing'),
+    plumber = require('gulp-plumber'),
+    browserSync = require('browser-sync').create(),
+    cssmin = require('gulp-cssmin'),
+    uncss = require('gulp-uncss'),
+    stripCssComments = require('gulp-strip-css-comments'),
+    uglify = require('gulp-uglify'),
+    cleanCSS = require('gulp-clean-css');
 
 var reload = browserSync.reload; // For manual browser reload.
 
@@ -30,8 +40,7 @@ var reload = browserSync.reload; // For manual browser reload.
 const AUTOPREFIXER_BROWSERS = [
     'last 2 version',
     '> 1%',
-    'ie >= 9',
-    'ie_mob >= 10',
+    'ie >= 11',
     'ff >= 30',
     'chrome >= 34',
     'safari >= 7',
@@ -45,18 +54,10 @@ var sass_config = {
   importer: importer,
   includePaths: [
     'node_modules/breakpoint-sass/stylesheets/',
-    'node_modules/singularitygs/stylesheets/singularitygs',
     'node_modules/modularscale-sass/stylesheets',
-    // 'node_modules/singularity-extras/stylesheets/',
     'node_modules/compass-mixins/lib/'
   ]
 };
-
-// Watch files paths.
-var styleWatchFiles         = 'sass/**/*.scss'; // Path to all *.scss files inside css folder and inside them.
-var styleSRC         = '*.css';
-var customJSWatchFiles      = 'js/*.js'; // Path to all custom JS files.
-var projectPHPWatchFiles    = '**/*.php'; // Path to all PHP files.
 
 gulp.task( 'browser-sync', function() {
   browserSync.init( {
