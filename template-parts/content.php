2139d26f6f3b41c7d4acaa8e1876800e59537fbe
Tyler Barnes <tyler@bare.ca>
2017-11-05 22:11:31 -0800

    Cleaning up template

--- template-parts/content.php
+++ template-parts/content.php
@@ -4,24 +4,15 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article <?php post_class(); ?>>
 
 		<header>
-			<?php the_title( '<h2 class="h2"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 		</header>
 
 	<div class="content">
-		<?php
-			the_content( sprintf(
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'adsystems' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'adsystems' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_content(); ?>
+		<?php wp_link_pages(); ?>
 	</div>
 
 </article>
