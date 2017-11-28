6a427e122b1258f05b7ac2e5c9ef245a0a416394
Robin <robin@bare.ca>
2017-11-28 09:30:47 -0800

    Plugin requirement + Screenshot

--- functions.php
+++ functions.php
@@ -3,3 +3,7 @@
 include('inc/functions/enqueue.php');
 include('inc/functions/menus.php');
 include('inc/functions/admin-css.php');
+
+// Install required plugins
+require_once get_template_directory() . '/inc/functions/register_required_plugins.php';
+require_once get_template_directory() . '/inc/functions/class-tgm-plugin-activation.php';
