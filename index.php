5d4f14770c021bd11079f7cf2da56213bc3512a4
Tyler <tyler@bare.ca>
2017-12-01 10:46:32 -0800

    Remove templates except for index.php

--- index.php
+++ index.php
@@ -8,11 +8,7 @@ get_header(); ?>
 
 <main class='main'>
 
-  <?php if(have_posts()): ?>
-    <?php while(have_posts()): the_post(); ?>
-      <?php the_content(); ?>
-    <?php endwhile; ?>
-  <?php endif; ?>
+  <?php include('template-objects/main-flexible-content.php'); ?>
 
 </main>
 
