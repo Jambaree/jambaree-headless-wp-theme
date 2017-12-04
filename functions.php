b23a414f5b2b5f072635761eb0d5b4cb2646d4a0
Tyler <tyler@bare.ca>
2017-12-04 15:22:48 -0800

    Add responsive images to functions.php

--- functions.php
+++ functions.php
@@ -13,3 +13,5 @@ include_once('inc/functions/register-required-plugins.php');
 include_once('inc/functions/required-plugins.php');
 
 include_once('inc/functions/acf-options-pages.php');
+
+include_once('inc/functions/responsive-images.php');
