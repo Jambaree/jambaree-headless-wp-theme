efdeb2e2119fdb73aeec66802a1bd597117c1589
Tyler <tyler@bare.ca>
2017-12-04 14:50:22 -0800

    Fix js includes

--- inc/functions/enqueue.php
+++ inc/functions/enqueue.php
@@ -1,11 +1,14 @@
 <?php
 
 function bareNakedEnqueue() {
+  $stylesheet_directory_uri = get_stylesheet_directory_uri();
+  
   wp_enqueue_style( 'styles', get_stylesheet_uri() );
 
   wp_enqueue_script( 'jquery' );
-  wp_enqueue_script('main-script', get_stylesheet_directory_uri() . '/js/menu.js', array('jquery'), null, false);
-  wp_enqueue_script('eqcss', get_stylesheet_directory_uri() . '/js/vendor/EQCSS.min.js', array(), null, true);
+
+  wp_enqueue_script('main-script', $stylesheet_directory_uri . '/inc/js/menu.js', array('jquery'), null, false);
+  wp_enqueue_script('eqcss', $stylesheet_directory_uri . '/inc/js/vendor/EQCSS.min.js', array(), null, true);
 }
 
 
