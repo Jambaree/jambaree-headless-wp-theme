0f9120f99375a00dc4380060363ee7c5117df4a0
Tyler <tyler@bare.ca>
2017-12-04 17:48:09 -0800

    Add wrapper to title

--- page-objects/title/title.php
+++ page-objects/title/title.php
@@ -1,7 +1,10 @@
-<?php
-$element_type = get_sub_field('element_type');
+<section class="grid-edges">
+  <?php
+  $element_type = get_sub_field('element_type');
 
-$format = '<%1$s class="'. get_sub_field('title_size') .'">'. get_sub_field('title') .'</%1$s>';
+  $format = '<%1$s class="'. get_sub_field('title_size') .'">'. get_sub_field('title') .'</%1$s>';
 
-echo sprintf($format, $element_type);
- ?>
+  echo sprintf($format, $element_type);
+   ?>
+
+</section>
