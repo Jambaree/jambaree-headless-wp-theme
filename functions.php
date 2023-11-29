4c23b9b1d791c00f2916cbd343db0e90c7b9a535
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-11-29 14:39:42 -0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 
 include_once('inc/functions/menus.php');
-include_once('inc/functions/admin-css.php');
+// include_once('inc/functions/admin-css.php');
 include_once('inc/functions/register-required-plugins.php');
 include_once('inc/functions/required-plugins.php');
