68ff3b378805eee742e0a53ba604eafc2631af27
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-11-29 19:41:56 -0800

    add_theme_support( 'custom-logo' )

--- functions.php
+++ functions.php
@@ -4,3 +4,5 @@ include_once('inc/functions/menus.php');
 // include_once('inc/functions/admin-css.php');
 include_once('inc/functions/register-required-plugins.php');
 include_once('inc/functions/required-plugins.php');
+
+add_theme_support( 'custom-logo' );
