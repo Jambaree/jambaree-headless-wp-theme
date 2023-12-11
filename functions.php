4798432ef721250760c2b12e3e3ea7c28e85e7e5
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-12-11 12:37:18 -0800

    1.2.0 - add theme support for post thumbnails

--- functions.php
+++ functions.php
@@ -6,3 +6,4 @@ include_once('inc/functions/register-required-plugins.php');
 include_once('inc/functions/required-plugins.php');
 
 add_theme_support( 'custom-logo' );
+add_theme_support( 'post-thumbnails' );
